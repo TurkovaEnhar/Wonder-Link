73f75fc7b5ace93f8ea82c9d54cc43dde37d5b54
Enar <Enar>
2025-05-15 15:02:18 +0300

    Naming change and getcomponent move to the awake on chips.cs

--- Assets/Scripts/Board/Chips/Chip.cs
+++ Assets/Scripts/Board/Chips/Chip.cs
@@ -10,11 +10,16 @@ namespace Board.Chips
         public ChipColor Color { get; private set; }
         public Tile ParentTile { get;  set; }
         private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
         public void Initialize(ChipColor color, Tile tile,Sprite sprite)
         {
             Color = color;
             ParentTile = tile;
-            _spriteRenderer = GetComponent<SpriteRenderer>();
             _spriteRenderer.sprite = sprite;
         }
         
